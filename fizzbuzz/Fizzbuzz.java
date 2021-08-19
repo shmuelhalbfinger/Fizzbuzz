272bfbc6f0ddb9fc23219de77b15bf38ac220864
Shalbfinger <Shalbfinger@softworksai.com>
2021-08-19 11:33:14 -0400

    Readded Files

---
+++ fizzbuzz/Fizzbuzz.java
@@ -0,0 +1,26 @@
+package fizzbuzz;
+
+public class Fizzbuzz {
+
+    public static void printFizzbuzz(int i) {
+        if (i % 3 == 0) {
+            if (i % 5 == 0) {
+                System.out.print(Integer.toString(i) + "- Fizz Buzz");
+            } else {
+                System.out.print(Integer.toString(i) + "- Fizz");
+            }
+        } else if (i % 5 == 0) {
+            System.out.print(Integer.toString(i) + "- Buzz");
+        } else {
+            System.out.print(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            printFizzbuzz(i);
+            System.out.println(",");
+        }
+    }
+
+}
