8e765c4f0ec1be76558bf8532613214b38ed14c9
Shalbfinger <Shalbfinger@softworksai.com>
2021-08-19 11:40:12 -0400

    Changed loop size again

--- fizzbuzz/Fizzbuzz.java
+++ fizzbuzz/Fizzbuzz.java
@@ -17,7 +17,7 @@ public class Fizzbuzz {
     }
 
     public static void main(String[] args) {
-        for (int i = 1; i <= 150; i++) {
+        for (int i = 1; i <= 200; i++) {
             printFizzbuzz(i);
             System.out.println(",");
         }
